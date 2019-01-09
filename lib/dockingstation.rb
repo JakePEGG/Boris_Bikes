ea9c3d7f60db9e6c14f5893afa824f902da2684e
JakePEGG <jake.a.atkin@gmail.com>
2019-01-09 15:02:38 +0000

    Step 10 - Creating & passing additional tests

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,20 +1,9 @@
 #A Domain Model is an abstract representation of the Objects within a system
 class DockingStation
-  # def initialize(release_bike)
-  # @release_bike = release_bike
-  # end
-
   def release_bike
-    # if bike == working then release_bike
-    # else
-    #   break
-    # CODE TO TEST IF BIKE RELEASED
-    # end
+    Bike.new
   end
 
-  #   def working_bike
-  #     # code to test if working
-  #   end
 end
 
 docking_station = DockingStation.new
--- spec/docking_Station_spec.rb
+++ spec/docking_Station_spec.rb
@@ -4,6 +4,17 @@ require "dockingstation.rb"
 # require_relative "../lib/feature_test.rb"
 
 describe DockingStation do
+
   it { is_expected.to respond_to :release_bike }
   # * a method within a class becomes and object hence "respond_to" checks for the object (:release_bike)
+  it "Gets a bike" do
+    expect(subject.release_bike).to (be_an_instance_of(Bike))
+  end
+
+  it "bike is working" do
+    expect(subject.release_bike.working?). to be true
+  end 
 end
+
+# gets a bike
+# subject == dockingstation in this example as good practice within ruby. Dockingstation is a class and subject makes an instance
