1d9b41f826bb03a75c77edb500646a1e1b40baad
JakePEGG <jake.a.atkin@gmail.com>
2019-01-09 15:13:48 +0000

    Solved previous test

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,13 +1,13 @@
 #A Domain Model is an abstract representation of the Objects within a system
 class DockingStation
   def release_bike
-    Bike.new
+    # Bike.new
   end
 
 end
 
-docking_station = DockingStation.new
-
-bike = docking_station.release_bike
+# docking_station = DockingStation.new
+#
+# bike = docking_station.release_bike
 
 # working = docking_station.working_bike
