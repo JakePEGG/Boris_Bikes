06d25f9cf13c81e756d8e2b46daff73166340532
Erin Stevenson <estevenson_94@live.com>
2019-01-11 14:59:59 +0000

    Added full? and empty? methods to dockingstation.rb

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -8,14 +8,23 @@ class DockingStation
   end
 
   def release_bike
-    fail 'No bikes available' if @bikes.count == 0
+    fail 'No bikes available' if empty?
     @bikes.pop
   end
 
   def dock(bike)
-   fail 'Docking station full' if @bikes.count >= 20
+   fail 'Docking station full' if full?
    @bikes << bike
- end
+   end
+
+  def full?
+   @bikes.count >= 20 
+  end
+
+  def empty?
+    @bikes.count == 0
+  end
+
 end
 
 
